68efe1cc25a6e125d3f158621d2632a5a67ec716
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-04-19 14:21:24 +0200

    Initial Commit

---
+++ DisclosureGroup/DisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclosureGroupApp.swift
+//  DisclosureGroup
+//
+//  Created by Ndayisenga Jean Claude on 19/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
