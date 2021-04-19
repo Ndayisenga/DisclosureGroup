27dc864707d76764d914086cf5aa2f6782d55ca5
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-04-19 15:44:37 +0200

    Creating a Disclosure

--- DisclosureGroup/ContentView.swift
+++ DisclosureGroup/ContentView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isExpanded = false
+    @State var isTermsExpanded = false
+    @State var isPrivacyExpanded = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        NavigationView {
+            
+            VStack {
+                
+                DisclosureGroup("Legal Stuffs", isExpanded: $isExpanded) {
+                    
+                    DisclosureGroup("Terms", isExpanded: $isTermsExpanded) {
+                    
+                    
+                    Text("Terms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and services").multilineTextAlignment(.leading)
+                }
+                .padding()
+                
+                
+                DisclosureGroup("Privacy ", isExpanded: $isPrivacyExpanded) {
+                    Text("Terms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and servicesTerms and services").multilineTextAlignment(.leading)
+                
+                }
+                .padding()
+                
+                }
+                .padding()
+                
+                
+                 Spacer()
+                
+        }
+            .navigationTitle("Disclosure Group")
+            
+        }
+     }
 }
 
 struct ContentView_Previews: PreviewProvider {
